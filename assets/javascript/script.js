1c1e8b002a248dcaffb8fc1e8c0d73f441eeea14
k2k <kadonna@wonderkul.com>
2019-06-30 16:32:09 -0500

    Added the on-click event for the images returned in the query but a little stuck getting them to toggle from still to animated.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,58 +1,41 @@
 //My javascript file
 
-// page displays with buttons labeled with animals
-//Add an animal - text field and submit button
-
-//create an array of the animals
-//traverse the array to create animal buttons
-//input field with submit button - on click adds animal to the array
-//each button links to giphy - displays 10 giphys of that keyword
-//clicking the still image toggles to the animated image and vice-versa (thurs class)
-
-//create an array of the animals
-var animals = ["Dogs", "Cats", "Penguins", "Ostrich", "Bear", "Hamster", "Birds", "Armadillo", "Cow", "Horse"];
+//create an array of the animals. These will be buttons displayed when the page loads
+var animals = ["Dogs", "Cats", "Penguins", "Ostrich", "Bear", "Hamster", "Birds", "Armadillo", "Cow", "Seal"];
 
 //Function to call animal pics from api
     function displayAnimals(){
+        $("#animalPics").empty();
         var animal = $(this).attr("data-name");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q="+animal+"&api_key=WvmR5JCtXlAswDhoqIJdtS4NfUv0963C&limit=10";
-
+        var image = "";
 
         $.ajax({ url: queryURL, method: "GET"})
           .then(function(AnimalGifData) {
 
               for (var i=0; i<AnimalGifData.data.length; i++){
-           //   console.log(AnimalGifData.data[i].images.fixed_height.url);
-           //   console.log(AnimalGifData.data[i].images.looping.mp4); 
 
-              //start dumping to html
-              var images = $("<img>");
-              images.attr("src", AnimalGifData.data[i].images.fixed_height.url);
+              //add images from api call to the page
+              images = $("<img>");
+              images.attr({"src": AnimalGifData.data[i].images.fixed_height_still.url, "data-still": AnimalGifData.data[i].images.fixed_height_still.url, "data-animate": AnimalGifData.data[i].images.looping.mp4, "data-state": "still"});
+              images.addClass("animalPics");
               $("#animalPics").append(images);
-
-              //attach content to the appropriate image tag
-            //  $("#animalPics" + i).append("AnimalGifData.data[i].images.fixed_height.url");
-
                       }
-
               });
-          }  
-
-// The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    //var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    //if (state === "still") {
-   //   $(this).attr("src", $(this).attr("data-animate"));
-   //   $(this).attr("data-state", "animate");
-   // } else {
-  //    $(this).attr("src", $(this).attr("data-still"));
-   //   $(this).attr("data-state", "still");
- //   }
-
-
-
+         
+//Clicking on gif activates the looping gif and vice-versa
+    $("#animalPics").on("click", function() {
+      //alert("This is an aleart");
+        var state = $(images).attr("data-state");
+          if (state === "still") {
+                $(images).attr("src", $(images).attr("data-animate"));
+                $(images).attr("data-state", "animate");
+          } else {
+                $(images).attr("src", $(images).attr("data-still"));
+                $(images).attr("data-state", "still");
+          }
+});
+}
 //create an array of the animals
 
 // page displays with buttons labeled with animals
@@ -72,7 +55,7 @@ var animals = ["Dogs", "Cats", "Penguins", "Ostrich", "Bear", "Hamster", "Birds"
               }
     }
 
-//on click event to create a new animal button
+//creates a new animal button when the user enters a new animal in the field
       $("#addAnimal").click(function(event){
           event.preventDefault();
 
@@ -86,3 +69,4 @@ var animals = ["Dogs", "Cats", "Penguins", "Ostrich", "Bear", "Hamster", "Birds"
     $(document).on("click", ".animal", displayAnimals);
 
 renderButtons();
+
